ba32e6962927849eaaa99a4f8472c8daa00b36d5
F34121094 <162594929+F34121094@users.noreply.github.com>
2024-10-26 23:42:18 +0800

    Add files via upload

---
+++ 程設（一）作業區/HomeWork_3/p5.cpp
@@ -0,0 +1,16 @@
+#include<string.h>
+#include<stdio.h>
+
+int main(){
+	char str1[50],str2[50],str3[50];
+	scanf("%s%s",str1,str2);
+	int l = strlen(str1);
+	for (int i = 0;i<l;i++){
+		str3[l-i-1] = str1[i];
+	}
+	str3[l] = '\0';
+	strcat(str2,str3);
+	printf("%s",str2);
+	
+	return 0;
+}
---
+++ 程設（一）作業區/HomeWork_3/p6.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main(){
+	int total;
+	int goal;
+	int time = 0;
+	scanf("%d",&goal);
+	while(1){
+		time ++;
+		int add;
+		scanf("%d",&add);
+		total += add;
+		if(total > goal){
+			break;
+		}
+	}
+	printf("Fried Pork!!!!\n");
+	printf("%d",time);
+	
+	return 0;
+}
