d7481407f0809e297196cb288847bbdca4a915d2
F34121094 <162594929+F34121094@users.noreply.github.com>
2024-10-28 15:23:21 +0800

    Add files via upload

--- 程設（一）作業區/HomeWork_3/p1.cpp
+++ 程設（一）作業區/HomeWork_3/p1.cpp
@@ -1,26 +1,19 @@
 #include<stdio.h>
+#include<math.h>
 
 int main(){
-	int a, b, c ;
-	scanf("%d %d %d",&a,&b,&c);
-	int rl=1; //1由左到右 
-	int q = b/a;
-	if (q%2 == 1){
-		rl = 2;
-	}else{
-		rl = 1;
+	int a,b,c;
+	scanf("%d%d%d",&a,&b,&c);
+	int total = floor(b/a);
+	int last = b - total*a;
+	int leftorright = (total)%2==0 ? 0:1;	//0由左到右 
+	if (leftorright == 0 && last >= c){
+		total ++;
+	}else if(leftorright ==1 && last >= a+1-c){
+		total ++;
 	}
-	int last = a % b;
-	if (rl == 1){
-		if (last >= c){
-		q += 1;
-		}
-	}else{
-		if (last >= (a-c+1)){
-			q += 1;
-		}
-	}
-	printf("%d",q);
+	printf("%d\n",total);
 	
 	return 0;
+	
 }
